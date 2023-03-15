077b8bc9f8708ad4a34820ac12c86cf1a9976365
Gustavo H. Fazam <112525498+GuFazam@users.noreply.github.com>
2023-03-15 20:31:57 -0300

    Criarei um segundo formulario

--- VariosFormularios/Form1.Designer.cs
+++ VariosFormularios/Form1.Designer.cs
@@ -33,10 +33,12 @@
             this.textBoxMultTexto = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btnLimpar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBoxInserir
             // 
+            this.textBoxInserir.BackColor = System.Drawing.Color.Linen;
             this.textBoxInserir.Location = new System.Drawing.Point(28, 51);
             this.textBoxInserir.Name = "textBoxInserir";
             this.textBoxInserir.Size = new System.Drawing.Size(237, 20);
@@ -44,27 +46,31 @@
             // 
             // btnInserir
             // 
+            this.btnInserir.BackColor = System.Drawing.Color.CornflowerBlue;
             this.btnInserir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInserir.ForeColor = System.Drawing.SystemColors.Window;
             this.btnInserir.Location = new System.Drawing.Point(271, 49);
             this.btnInserir.Name = "btnInserir";
             this.btnInserir.Size = new System.Drawing.Size(87, 23);
             this.btnInserir.TabIndex = 1;
             this.btnInserir.Text = "Inserir";
-            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.UseVisualStyleBackColor = false;
             this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
             // 
             // textBoxMultTexto
             // 
+            this.textBoxMultTexto.BackColor = System.Drawing.Color.Linen;
             this.textBoxMultTexto.Location = new System.Drawing.Point(27, 78);
             this.textBoxMultTexto.Multiline = true;
             this.textBoxMultTexto.Name = "textBoxMultTexto";
-            this.textBoxMultTexto.Size = new System.Drawing.Size(331, 306);
+            this.textBoxMultTexto.Size = new System.Drawing.Size(331, 297);
             this.textBoxMultTexto.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(361, 24);
@@ -73,19 +79,38 @@
             // 
             // btnLimpar
             // 
+            this.btnLimpar.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnLimpar.FlatAppearance.BorderColor = System.Drawing.SystemColors.WindowText;
             this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLimpar.Location = new System.Drawing.Point(28, 403);
+            this.btnLimpar.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnLimpar.Location = new System.Drawing.Point(27, 381);
             this.btnLimpar.Name = "btnLimpar";
             this.btnLimpar.Size = new System.Drawing.Size(330, 23);
             this.btnLimpar.TabIndex = 4;
             this.btnLimpar.Text = "Limpar";
-            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.WindowText;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Window;
+            this.button1.Location = new System.Drawing.Point(27, 410);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(330, 28);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Abrir Segundo Formulário";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(398, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnLimpar);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxMultTexto);
@@ -105,6 +130,7 @@
         private System.Windows.Forms.TextBox textBoxMultTexto;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- VariosFormularios/Form1.cs
+++ VariosFormularios/Form1.cs
@@ -19,16 +19,24 @@ namespace VariosFormularios
 
         private void btnInserir_Click(object sender, EventArgs e)
         {
-            if (textBoxInserir.Text == "")
+            if (textBoxInserir.Text == "") // este if está validando o textBoxInserir caso o btnInserir seja clicado sem nenhum texto inserido nele
             {
-                MessageBox.Show("Por favor insira um nome no campo!!!", "Aviso!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                textBoxInserir.Focus();
+                MessageBox.Show("Por favor insira um nome no campo!!!", "Aviso!", MessageBoxButtons.OK, MessageBoxIcon.Warning); // cria uma mensagem de aviso
+                textBoxInserir.Focus(); // Direciona o foco para textBoxInserir
                 return;
             }
+            //else é opcional por causa do Return
 
-            textBoxMultTexto.Text += textBoxInserir.Text + ", ";
-            textBoxInserir.Clear();
-            textBoxInserir.Focus();
+            textBoxMultTexto.Text += textBoxInserir.Text + ", "; // += adiciona o valor à direita à variável ou propriedade à esquerda e atribui o resultado à variável ou à propriedade à esquerda, o + sózino tá concatenando
+            textBoxInserir.Clear(); // está linha limpa o textBoxInserir
+            textBoxInserir.Focus(); // Direciona o foco para textBoxInserir
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            textBoxInserir.Clear(); // está linha limpa o textBoxInserir
+            textBoxMultTexto.Clear(); // está linha limpa o textBoxMultiTexto
+            textBoxInserir.Focus(); // Direciona o foco para textBoxInserir
         }
     }
 }
