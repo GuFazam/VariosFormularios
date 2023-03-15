cdc4581234c3e3ddc151ff1db7b22fc57b818ddb
Gustavo H. Fazam <112525498+GuFazam@users.noreply.github.com>
2023-03-15 19:52:47 -0300

    Adicionar arquivos de projeto.

---
+++ VariosFormularios/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace VariosFormularios
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInserir = new System.Windows.Forms.TextBox();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.textBoxMultTexto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxInserir
+            // 
+            this.textBoxInserir.Location = new System.Drawing.Point(28, 51);
+            this.textBoxInserir.Name = "textBoxInserir";
+            this.textBoxInserir.Size = new System.Drawing.Size(237, 20);
+            this.textBoxInserir.TabIndex = 0;
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInserir.Location = new System.Drawing.Point(271, 49);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(87, 23);
+            this.btnInserir.TabIndex = 1;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // textBoxMultTexto
+            // 
+            this.textBoxMultTexto.Location = new System.Drawing.Point(27, 78);
+            this.textBoxMultTexto.Multiline = true;
+            this.textBoxMultTexto.Name = "textBoxMultTexto";
+            this.textBoxMultTexto.Size = new System.Drawing.Size(331, 306);
+            this.textBoxMultTexto.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(361, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Digite o nome dos seus convidados!!!";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(28, 403);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(330, 23);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 450);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxMultTexto);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.textBoxInserir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInserir;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.TextBox textBoxMultTexto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ VariosFormularios/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VariosFormularios
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            if (textBoxInserir.Text == "")
+            {
+                MessageBox.Show("Por favor insira um nome no campo!!!", "Aviso!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBoxInserir.Focus();
+                return;
+            }
+
+            textBoxMultTexto.Text += textBoxInserir.Text + ", ";
+            textBoxInserir.Clear();
+            textBoxInserir.Focus();
+        }
+    }
+}
